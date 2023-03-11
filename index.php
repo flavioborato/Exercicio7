d15d4a57be2e6845a919c92620efaa244e080bbc
Flavio Fernando Borato <toraxico@msn.com>
2023-03-11 12:22:13 -0300

    Modificação
    Teste de Codigo

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
         $arr["peso"] = filter_input(INPUT_POST,'peso',FILTER_SANITIZE_SPECIAL_CHARS);
 
         $imc["valor"]= $arr["peso"]/(($arr["altura"]*$arr["altura"])/10000);
-        if($imc["valor"]<18.5){
+        if($imc["valor"]<18.50){
             $imc["classificacao"]="MAGRO";
             $imc["grau"]="0";
-        }elseif($imc["valor"]<24.9){
+        }elseif($imc["valor"]<24.90){
             $imc["classificacao"]="NORMAL";
             $imc["grau"]="0";
-        }elseif($imc["valor"]<29.9){
+        }elseif($imc["valor"]<29.90){
             $imc["classificacao"]="SOBREPESO";
             $imc["grau"]="I";
-        }elseif($imc["valor"]<39.9){
+        }elseif($imc["valor"]<39.90){
             $imc["classificacao"]="OBESIDADE";
             $imc["grau"]="II";
         }else{
@@ -26,20 +26,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	 	 <title>Formula IMC</title>
+	 	 <title>Formula IMC Unigran</title>
 </head>
 <body>
+
  <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
- Nome: <input type="text" name="nome"><br>
- Altura: <input type="number" name="altura"><br>
- Peso: <input type="number" name="peso"><br>
+ Nome:- <input type="text" name="nome"><br>
+ Altura:- <input type="number" name="altura"><br>
+ Peso:- <input type="number" name="peso"><br>
  <input type="submit">
 
  <?php
+
     if(isset($imc["valor"])){
-        echo "<h2>O usuário ".$arr["nome"]." com peso ".$arr["peso"]." e altura ".$arr["altura"]."</h2>";
-        echo "<h2>Foi classificado como ".$imc["classificacao"]." Grau de obesidade ".$imc["grau"]." do imc = ".$imc["valor"]."</h2>";
+        echo "<h3> O usuário ".$arr["nome"]." com peso ".$arr["peso"]." e altura ".$arr["altura"]."</h3>";
+        echo "<h3> Está classificado como ".$imc["classificacao"]." Grau de obesidade ".$imc["grau"]." do imc = ".$imc["valor"]."</h3>";
     }
+
  ?>
  </form>
 </body>
